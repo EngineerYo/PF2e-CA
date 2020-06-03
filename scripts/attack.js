9a1b032108eb31d64936f41320c6250c924fc124
EngineerYo <willcrain96@gmail.com>
2020-06-03 12:29:39 -0700

    Fixed crit logic; Updated attackLookup

--- scripts/attack.js
+++ scripts/attack.js
@@ -1,11 +1,13 @@
-import {sendMessage} from './sendMessagae.js'
-import attackLookup from './../attackLookup.json'
-
-
-attackLookup = JSON.parse(attackLookup)
+import {sendMessage} from './sendMessage.js'
 
 export function attack(message) {
-    let outMsg = `<div><h3 style='border-bottom: 3px solid black'>Attack</h3><div>`
+    let naturalRoll = message.roll.dice[0].rolls[0].roll
+    let rollTotal = message.roll.total
+
+    let outMsg = `<div style='border-bottom: 3px solid black; padding-right: 5px; margin-bottom: 0px;'>`
+    outMsg += `<h3 style='margin-bottom: 0px; display: inline-block;'>Attack</h3>`
+    outMsg += `<h3 style='margin-bottom: 0px; float: right;'>${rollTotal}</h3>`
+    outMsg += `</div>`
 
     const CRITICAL_SUCCESS = 1
     const SUCCESS =     0
@@ -13,54 +15,119 @@ export function attack(message) {
     const CRITICAL_FAILURE = -2
 
     game.user.targets.forEach(target => {
-        let naturalRoll = message.roll.dice[0].rolls[0].roll
-        let rollTotal = message.roll.total
         let actorAC = target.actor.data.data.attributes.ac.value
 
         let currentStep = null
 
-        if (naturalRoll != 20 && naturalRoll != 1) {
-            if (rollTotal >= actorAC + 10) {
-                currentStep = CRITICAL_SUCCESS
-            } 
-            else if (rollTotal >= actorAC) {
-                currentStep = SUCCESS
-            }
-            else if (rollTotal <= actorAC - 10) {
-                currentStep = CRITICAL_FAILURE
-            }
-            else {
-                currentStep = FAILURE
-            }
+        if (rollTotal >= actorAC + 10) {
+            currentStep = CRITICAL_SUCCESS
+        } 
+        else if (rollTotal >= actorAC) {
+            currentStep = SUCCESS
+        }
+        else if (rollTotal <= actorAC - 10) {
+            currentStep = CRITICAL_FAILURE
         }
         else {
-            if (naturalRoll == 20) {
-                currentStep += 1
-            }
-            else if (naturalRoll == 1) {
-                currentStep -= 1
-            }
-        } 
+            currentStep = FAILURE
+        }
+
+        if (naturalRoll == 20) {
+            currentStep += 1
+        }
+        else if (naturalRoll == 1) {
+            currentStep -= 1
+        }
 
         let finalResult = Math.clamp(currentStep, CRITICAL_FAILURE, CRITICAL_SUCCESS)
+        console.log(`finalResult\t${finalResult}`)
 
-        let heroName = game.user.character
+        let heroName = game.user.character.name
         let posHeroName = heroName
         if (heroName[heroName.length - 1] == 's') {
-            posHeroName = heroName.substring(0, heroName.length)
+            posHeroName += `'`
+        }
+        else {
+            posHeroName += `'s`
         }
 
         let successBy = Math.clamp(rollTotal - actorAC, -10, 10)
         if (naturalRoll == 20) {
-            successBy = successBy + 10
+            successBy = successBy + 9
         }
         else if (naturalRoll == 1) {
-            successBy = successBy - 10
+            successBy = successBy - 9
         }
 
+        let attackLookup = [
+            {
+                low:    null,
+                high:   -10,
+                color:  '#F4CCCC',
+                messages: [
+                    {weight: 10, text: `Is ${heroName} even trying?`},
+                    {weight: 10, text: `A supreme disappointment.`},
+                    {weight:  1, text: `Sad and noob!`},
+                    {weight: 10, text: `I hope no one was watching that...`},
+                    {weight:  1, text: `${heroName} has small peepee.`}
+                ]
+            },
+            {
+                low:    -9,
+                high:   -5,
+                color:  '#F9CB9C',
+                messages: [
+                    {weight: 10, text: `The attack falters against the foe's superior defenses.`},
+                    {weight: 10, text: `${heroName} will have to try harder than that.`}
+                ]
+            },
+            {
+                low:    -4,
+                high:   -1,
+                color:  '#FFF2CC',
+                messages: [
+                    {weight: 10, text: `Heroes never miss, but sometimes their attacks are dodged. Like this one.`},
+                    {weight: 10, text: `A near thing, but still a miss.`},
+                    {weight: 10, text: `${heroName} narrowly misses their target.`}
+                ]
+            },
+            {
+                low:    0,
+                high:   4,
+                color:  '#D9EAD3',
+                messages: [
+                    {weight: 10, text: `${posHeroName} strike connects, but just barely.`},
+                    {weight: 10, text: `A lucky hit, nothing more.`}
+                ]
+            },
+            {
+                low:    5,
+                high:   9,
+                color:  '#D0E0E3',
+                messages: [
+                    {weight: 10, text: `${posHeroName} strike was well placed.`},
+                    {weight: 10, text: `Another solid hit by ${posHeroName}.`}
+                ]
+            },
+            {
+                low:    10,
+                high:   null,
+                color:  '#C9DAF8',
+                messages: [
+                    {weight: 10, text: `${heroName} makes a critical strike against their foe!`},
+                    {weight: 10, text: `Mortality clarified in a single strike!`},
+                    {weight: 10, text: `Decimated! Obliterated! Annhiliated!`},
+                    {weight: 10, text: `Destruction awaits this one!`}
+                ]
+            }
+        ]
+
+        let targetRegime = null
         let selectedMessage = null
+
         for (let regime of attackLookup) {
             if ( (regime.high == null || successBy <= regime.high) && (regime.low == null || successBy >= regime.low) ) {
+                targetRegime = regime
                 // Sum weights in regime
                 let weightSum = 0
                 for (let message of regime.messages) {
@@ -68,7 +135,7 @@ export function attack(message) {
                 }
 
                 let randNum = Math.random()*weightSum
-                for (let message of target.messages) {
+                for (let message of regime.messages) {
                     if (randNum <= message.weight) {
                         selectedMessage = message.text
 
@@ -84,29 +151,35 @@ export function attack(message) {
         }
 
 
-        outMsg += `<div class = 'targetPicker' data-target="${target.data._id}" data-hitType="cm">`
-        outMsg += `<div style = "color: #131516; margin-top: 4px;"`
-        outMsg += `<b>${target.name}</b>`
+        let textLookup = {
+             '1':   'Critical hit!',
+             '0':            'Hit!',
+            '-1':            'Miss!',
+            '-2':   'Critical miss!'
+        }
+        
+        outMsg += `<div class = 'targetPicker' data-target="${target.data._id}" data-hitType="cm" style = "background-color: ${targetRegime.color}; padding-top: 4px; padding-bottom: 2px; padding-left: 5px; padding-right: 5px; border-bottom: 2px solid black;">`
+        outMsg += `<div style = "color: #131516;">`
+        outMsg += `<b style='display: inline-block'>${target.name}</b>`
+        outMsg += `<b style='float: right'>${textLookup[finalResult]}</b>`
         outMsg += `</div>`
-        outMsg += `<div style="border-bottom: 2px solid black; color: #131516; padding-bottom: 4px;">`
-        if (currentStep == CRITICAL_SUCCESS) {
-            outMsg += `<b style="color: #4C7D4C>`
-            outMsg += `Critically hit! ${selectedMessage}`
-            outMsg += `</b>`
+        outMsg += `<div style="color: #131516;">`
+
+        if (finalResult === CRITICAL_SUCCESS) {
+            console.log('Crit success')
+            outMsg += `${selectedMessage}`
         }
-        else if (currentStep == SUCCESS) {
-            outMsg += `Hit! ${selectedMessage}`
+        else if (finalResult === SUCCESS) {
+            outMsg += `${selectedMessage}`
         }
-        else if (currentStep == FAILURE) {
-            outMsg += `Miss! ${selectedMessage}`
+        else if (finalResult === FAILURE) {
+            outMsg += `${selectedMessage}`
         }
-        else if (currentStep == CRITICAL_FAILURE) {
-            outMsg += `<b style="color: #990000>`
-            outMsg += `Critically missed! ${selectedMessage}`
-            outMsg += `</b>`
+        else if (finalResult === CRITICAL_FAILURE) {
+            outMsg += `${selectedMessage}`
         }
         else {
-            console.log(`currentStep out of bounds\t${currentStep}`)
+            console.log(`finalResult out of bounds\t${finalResult   }`)
         }
 
         outMsg += `</div>`
