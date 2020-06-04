a1ffb83760ae37844a021e0ba069f94e225c2829
EngineerYo <willcrain96@gmail.com>
2020-06-03 23:38:00 -0700

    Update PF2E-CA-main.js

--- PF2E-CA-main.js
+++ PF2E-CA-main.js
@@ -1,14 +1,15 @@
 import {attack} from './scripts/attack.js'
-import {save} from './scripts/save.js'
+// import {save} from './scripts/save.js'
 
 Math.clamp = function(n, min, max) {
 	return Math.max(min, Math.min(n, max))
 }
 
 Hooks.on('createChatMessage', (message) => {
-	console.log('Hi!')
-
 	if (game.combat == null || game.users.entities.find(user => user.isGM)._id || game.combat.combatant.players[0].data._id === game.user._id) {
+		if (message.data.flavor == undefined) {
+			return
+		}
 		if (message.data.flavor.includes('Attack Roll') && message.data.user == game.user.data._id) {
 			attack(message)
 		}
